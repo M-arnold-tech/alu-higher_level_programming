2326894a95e196721dc7bb1dda22e4c6b85207b6
M-arnold-tech <a.mugabo2@alustudent.com>
2025-03-20 16:53:30 -0700

    fn change

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""Python script that fetches http://0.0.0.0:5050/status"""
+"""Python script that fetches https://intranet.hbtn.io/status"""
 
 import urllib.request
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
+    urls = ["https://intranet.hbtn.io/status", "http://0.0.0.0:5050/status"]
 
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+    for url in urls:
+        try:
+            with urllib.request.urlopen(url) as response:
+                html = response.read()
+                print("Body response:")
+                print("\t- type: {}".format(type(html)))
+                print("\t- content: {}".format(html))
+                print("\t- utf8 content: {}".format(html.decode("utf-8")))
+            break  # Stop once the correct URL works
+        except Exception as e:
+            pass  # Try the next URL if one fails
