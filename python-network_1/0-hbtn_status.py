fb436087a5986af5c7b2b6a3af270d29b5a5d675
M-arnold-tech <a.mugabo2@alustudent.com>
2025-03-20 16:40:28 -0700

    changing the code

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,9 +2,13 @@
 """ Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
-
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    url = 'https://intranet.hbtn.io/status'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+
+    req = urllib.request.Request(url, headers=headers)
+
+    with urllib.request.urlopen(req) as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
