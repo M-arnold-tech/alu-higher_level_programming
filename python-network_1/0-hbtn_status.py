32f459c656ac81d4ce6a6a0c66f2feca03af90e5
M-arnold-tech <a.mugabo2@alustudent.com>
2025-03-20 16:48:50 -0700

    3rd change

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
+"""Python script that fetches http://0.0.0.0:5050/status"""
+
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'  # Make sure the URL is correct
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-    }
+    url = "http://0.0.0.0:5050/status"
 
-    req = urllib.request.Request(url, headers=headers)
-
-    try:
-        with urllib.request.urlopen(req) as response:
-            html = response.read()
-            print('Body response:')
-            print('\t- type: {}'.format(type(html)))
-            print('\t- content: {}'.format(html))
-            print('\t- utf8 content: {}'.format(html.decode("utf-8")))
-    except urllib.error.HTTPError as e:
-        print(f"HTTP Error: {e.code} - {e.reason}")
-    except urllib.error.URLError as e:
-        print(f"URL Error: {e.reason}")
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
