3464286cadfc25da1d35a164f516fb81cd633f7f
M-arnold-tech <a.mugabo2@alustudent.com>
2025-03-20 16:58:57 -0700

    final change

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,16 +4,14 @@
 import urllib.request
 
 if __name__ == "__main__":
-    urls = ["https://intranet.hbtn.io/status", "http://0.0.0.0:5050/status"]
+    url = "https://intranet.hbtn.io/status"
+    headers = {"User-Agent": "Mozilla/5.0"}
 
-    for url in urls:
-        try:
-            with urllib.request.urlopen(url) as response:
-                html = response.read()
-                print("Body response:")
-                print("\t- type: {}".format(type(html)))
-                print("\t- content: {}".format(html))
-                print("\t- utf8 content: {}".format(html.decode("utf-8")))
-            break  # Stop once the correct URL works
-        except Exception as e:
-            pass  # Try the next URL if one fails
+    req = urllib.request.Request(url, headers=headers)
+
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
