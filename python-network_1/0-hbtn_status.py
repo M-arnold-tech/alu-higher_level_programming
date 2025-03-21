4b71756138a2008b3bd475391324664002d53ad6
M-arnold-tech <a.mugabo2@alustudent.com>
2025-03-20 17:03:09 -0700

    poject for network

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,13 +5,20 @@ import urllib.request
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
 
     req = urllib.request.Request(url, headers=headers)
 
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+    try:
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(html)))
+            print("\t- content: {}".format(html))
+            print("\t- utf8 content: {}".format(html.decode("utf-8")))
+    except urllib.error.HTTPError as e:
+        print(f"HTTP Error: {e.code} - {e.reason}")
+    except urllib.error.URLError as e:
+        print(f"URL Error: {e.reason}")
